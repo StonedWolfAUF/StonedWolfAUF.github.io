c92f077dac2c75e977295ed28fff03481e11ecc1
gitaleph <github@aleph.zone>
2023-05-05 21:04:21 +0300

    main

---
+++ js/script.js
@@ -0,0 +1,42 @@
+﻿(function ($) {
+	$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+       event.preventDefault();
+       var id  = $(this).attr('href'),
+          elem = $(id).offset().top;
+       $('body,html').animate({scrollTop: elem}, 1500);
+       $('#page').removeClass('open-menu');
+    });
+
+    $('.btn-scroll').on("click", function (event) {
+       event.preventDefault();
+       var id  = $(this).attr('data-href'),
+          elem = $(id).offset().top;
+       $('body,html').animate({scrollTop: elem}, 1500);
+    });
+
+    $('#menu-triger').on('click', function(event) {
+      event.preventDefault();
+      var elem = $('#page');
+
+      if(elem.hasClass('open-menu')){
+        elem.removeClass('open-menu');
+      }
+      else{
+        elem.addClass('open-menu');
+      }
+    });
+	});
+
+	$(window).scroll(function(){
+		var scrolled = $(window).scrollTop(),
+    		height = $(window).height();
+
+  	if(scrolled > 5){
+  		$('#page').addClass('scrolled');
+  	}
+  	else{
+  		$('#page').removeClass('scrolled');
+  	}
+	});
+})(jQuery);
